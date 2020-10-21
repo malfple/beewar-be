c45b83f677a220f663fad51620219097632a4cf7
Christopher <christopher.samuel@shopee.com>
2020-10-21 16:03:31 +0700

    add graceful shutdown

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-	"strconv"
-
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
 )
 
 func Ping(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +31,34 @@ func main() {
 	router.Use(AccessLogMiddleware)
 	router.Use(cors.Default().Handler)
 
-	fmt.Printf("Starting server at port %d\n", port)
-	if err := http.ListenAndServe(":" + strconv.FormatInt(port, 10), router); err != nil {
-		fmt.Println("fail to start server")
+	server := &http.Server{
+		Addr: ":" + strconv.FormatInt(port, 10),
+		WriteTimeout: time.Second * 15,
+		ReadTimeout: time.Second * 15,
+		IdleTimeout: time.Second * 60,
+		Handler: router,
 	}
+
+	go func() {
+		fmt.Printf("Starting server at port %d\n", port)
+		if err := server.ListenAndServe(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	// graceful shutdown
+	c := make(chan os.Signal, 1)
+	// quit when SIGINT (ctrl + c)
+	signal.Notify(c, os.Interrupt)
+
+	// block until receive signal
+	<-c
+
+	// create deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15)
+	defer cancel()
+	// wait for connections to close or until deadline
+	server.Shutdown(ctx)
+	fmt.Println("shutting down!")
+	os.Exit(0)
 }
