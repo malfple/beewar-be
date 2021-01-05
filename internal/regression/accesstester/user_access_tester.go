99ed800b876324779eae30e95f016d39ff7b8d49
Christopher <christopher.samuel@shopee.com>
2021-01-05 14:40:18 +0700

    add batch user get by id
    also add sqlx lib -> for sql utilities
    also update regression test suite

--- internal/regression/accesstester/user_access_tester.go
+++ internal/regression/accesstester/user_access_tester.go
@@ -3,6 +3,7 @@ package accesstester
 import (
 	"gitlab.com/otqee/otqee-be/internal/access"
 	"gitlab.com/otqee/otqee-be/internal/logger"
+	"go.uber.org/zap"
 )
 
 // TestUserAccess runs regression tests for user access
@@ -13,9 +14,26 @@ func TestUserAccess() bool {
 	if err := access.CreateUser(username+"@somemail.com", username, "password"); err != nil {
 		return false
 	}
+	username2 := "username2"
+	if err := access.CreateUser(username2+"@somemail.com", username2, "password"); err != nil {
+		return false
+	}
 	// test user query
 	user := access.QueryUserByUsername(username)
 	if user == nil {
+		return false
+	}
+	user2 := access.QueryUserByUsername(username2)
+	if user2 == nil {
+		return false
+	}
+	users := access.QueryUsersByID([]uint64{user2.ID, user.ID})
+	if len(users) != 2 {
+		logger.GetLogger().Error("expected 2 users", zap.Int("actual", len(users)))
+		return false
+	}
+	if users[0].ID != user2.ID || users[1].ID != user.ID {
+		logger.GetLogger().Error("wrong order when batch query user")
 		return false
 	}
 	if !access.IsExistUserByID(user.ID) {
