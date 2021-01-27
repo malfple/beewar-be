ebf1283029254b1e62b33678836eb0d9ccf31a78
Christopher Samuel <noogai886@yahoo.com>
2021-01-27 09:46:35 +0700

    add database address to mysql config

--- configs/configs.go
+++ configs/configs.go
@@ -15,6 +15,9 @@ const (
 	// EnvDatabasePassword defines an env variable name
 	EnvDatabasePassword     = "DATABASE_PASSWORD"
 	defaultDatabasePassword = "malfplemac"
+	// EnvDatabaseAddress defines an env variable name
+	EnvDatabaseAddress     = "DATABASE_ADDR"
+	defaultDatabaseAddress = ":3306"
 	// EnvDatabaseName defines an env variable name
 	EnvDatabaseName     = "DATABASE_NAME"
 	defaultDatabaseName = "beewar"
@@ -29,9 +32,10 @@ func GetServerAddress() string {
 }
 
 // GetDatabaseConfig returns database config: (user, password, database_name)
-func GetDatabaseConfig() (string, string, string) {
+func GetDatabaseConfig() (string, string, string, string) {
 	user := os.Getenv(EnvDatabaseUser)
 	pass := os.Getenv(EnvDatabasePassword)
+	addr := os.Getenv(EnvDatabaseAddress)
 	db := os.Getenv(EnvDatabaseName)
 	if user == "" {
 		user = defaultDatabaseUser
@@ -39,14 +43,17 @@ func GetDatabaseConfig() (string, string, string) {
 	if pass == "" {
 		pass = defaultDatabasePassword
 	}
+	if addr == "" {
+		addr = defaultDatabaseAddress
+	}
 	if db == "" {
 		db = defaultDatabaseName
 	}
-	return user, pass, db
+	return user, pass, addr, db
 }
 
 // GetDatabaseMySQLDataSourceName returns user, password, database_name combined into a convenient string for mysql
 func GetDatabaseMySQLDataSourceName() string {
-	user, pass, db := GetDatabaseConfig()
-	return user + ":" + pass + "@/" + db
+	user, pass, addr, db := GetDatabaseConfig()
+	return user + ":" + pass + "@(" + addr + ")/" + db
 }
