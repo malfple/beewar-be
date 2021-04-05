8412434ac9a0d9482ec8359b804723fab65ef151
Christopher Samuel <noogai886@yahoo.com>
2021-04-05 18:08:42 +0800

    add password support when create game

--- internal/controller/gamemanager/game_crud.go
+++ internal/controller/gamemanager/game_crud.go
@@ -3,6 +3,9 @@ package gamemanager
 import (
 	"errors"
 	"gitlab.com/beewar/beewar-be/internal/access"
+	"gitlab.com/beewar/beewar-be/internal/logger"
+	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // mainly contains controllers for setup-ing game and deleting
@@ -12,13 +15,22 @@ var (
 	errGameDoesNotExist = errors.New("game does not exist")
 )
 
-// CreateGame creates a new game with the given map id
-func CreateGame(mapID uint64) (uint64, error) {
+// CreateGame creates a new game with the given map id. If password is provided, it will be bcrypt-ed
+func CreateGame(mapID uint64, password string) (uint64, error) {
 	mapModel := access.QueryMapByID(mapID)
 	if mapModel == nil {
 		return 0, errMapDoesNotExist
 	}
-	return access.CreateGameFromMap(mapModel, "")
+	var passwordHash = ""
+	if password != "" {
+		passwordHashByte, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			logger.GetLogger().Error("error bcrypt", zap.Error(err))
+			return 0, err
+		}
+		passwordHash = string(passwordHashByte)
+	}
+	return access.CreateGameFromMap(mapModel, passwordHash)
 }
 
 // RegisterForGame registers/links user to game
