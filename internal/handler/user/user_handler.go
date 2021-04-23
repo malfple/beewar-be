8fcb869e528f904d68f773825ebbbe99a234a314
Christopher Samuel <noogai886@yahoo.com>
2021-04-23 16:57:06 +0800

    add /user/list

--- internal/handler/user/user_handler.go
+++ internal/handler/user/user_handler.go
@@ -5,4 +5,5 @@ import "github.com/gorilla/mux"
 // RegisterUserRouter builds router for user related stuff
 func RegisterUserRouter(router *mux.Router) {
 	router.HandleFunc("/get_by_username", HandleUserGetByUsername).Methods("GET")
+	router.HandleFunc("/list", HandleUserList).Methods("GET")
 }
