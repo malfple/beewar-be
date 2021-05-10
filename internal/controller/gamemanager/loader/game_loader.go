b71457a3d91d7f5c18fd507a47a621fafbb72bc9
Christopher Samuel <noogai886@yahoo.com>
2021-05-10 22:42:44 +0800

    moved game loader method: join function to its own file

--- internal/controller/gamemanager/loader/game_loader.go
+++ internal/controller/gamemanager/loader/game_loader.go
@@ -10,7 +10,6 @@ import (
 	"gitlab.com/beewar/beewar-be/internal/controller/gamemanager/objects"
 	"gitlab.com/beewar/beewar-be/internal/logger"
 	"go.uber.org/zap"
-	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -181,50 +180,6 @@ func (gl *GameLoader) GameData() *message.GameMessage {
 			Players: players,
 		},
 	}
-}
-
-// handles player join
-func (gl *GameLoader) handleJoin(msg *message.GameMessage) (*message.GameMessage, bool) {
-	if gl.Status != GameStatusPicking {
-		return message.GameErrorMessage(errMsgGameNotInPicking), false
-	}
-	data := msg.Data.(*message.JoinMessageData)
-	if data.PlayerOrder < 1 || int(data.PlayerOrder) > gl.PlayerCount {
-		return message.GameErrorMessage(errMsgPlayerOrderInvalid), false
-	}
-	if gl.GameUsers[data.PlayerOrder-1].UserID != 0 { // taken haha
-		return message.GameErrorMessage(errMsgPlayerOrderTaken), false
-	}
-	if _, ok := gl.UserIDToPlayerMap[msg.Sender]; ok {
-		return message.GameErrorMessage(errMsgAlreadyJoined), false
-	}
-	if gl.Password != "" {
-		if err := bcrypt.CompareHashAndPassword([]byte(gl.Password), []byte(data.Password)); err != nil {
-			return message.GameErrorMessage(errMsgWrongPassword), false
-		}
-	}
-	// pass join validation
-	if err := access.CreateGameUser(gl.ID, msg.Sender, data.PlayerOrder); err != nil {
-		return message.GameErrorMessage(err.Error()), false
-	}
-	gl.GameUsers[data.PlayerOrder-1] = access.QueryGameUser(gl.ID, msg.Sender)
-	gl.Users[data.PlayerOrder-1] = access.QueryUsersByID([]uint64{msg.Sender})[0]
-	gl.Users[data.PlayerOrder-1].Password = "nope..."
-	gl.UserIDToPlayerMap[msg.Sender] = int(data.PlayerOrder)
-	gl.checkGameStart()
-	return &message.GameMessage{
-		Cmd:    msg.Cmd,
-		Sender: msg.Sender,
-		Data: &message.JoinMessageDataExt{
-			Player: &message.Player{
-				UserID:      msg.Sender,
-				PlayerOrder: data.PlayerOrder,
-				FinalRank:   0,
-				FinalTurns:  0,
-				User:        gl.Users[data.PlayerOrder-1],
-			},
-		},
-	}, true
 }
 
 func (gl *GameLoader) checkGameStart() {
