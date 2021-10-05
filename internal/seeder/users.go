c925076564cb9ec05e6b874679cf1d5afd916764
Christopher Samuel <noogai886@yahoo.com>
2021-10-05 22:59:15 +0800

    change seeded users to more neutral usernames

--- internal/seeder/users.go
+++ internal/seeder/users.go
@@ -5,9 +5,9 @@ import "gitlab.com/beewar/beewar-be/internal/controller/auth"
 // SeedUsers inserts default users
 func SeedUsers() bool {
 	_ = auth.Register("beebot", "beebot", "beebotbeebot")
-	_ = auth.Register("malfple@user.com", "malfple", "malfplesecret")
-	_ = auth.Register("rapel@user.com", "rapel", "rapelsecret")
-	_ = auth.Register("sebas@user.com", "sebas", "sebassecret")
-	_ = auth.Register("kyon@user.com", "kyon", "kyonsecret")
+	_ = auth.Register("test1@user.com", "test1", "testtest")
+	_ = auth.Register("test2@user.com", "test2", "testtest")
+	_ = auth.Register("test3@user.com", "test3", "testtest")
+	_ = auth.Register("test4@user.com", "test4", "testtest")
 	return true
 }
