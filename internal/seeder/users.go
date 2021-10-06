e26a2a29f6e1232db06a96b44d40023fd47def76
malfple <noogai886@yahoo.com>
2021-10-06 16:09:47 +0800

    [seeder] fix user seeder throwing errors
    user seeder no longer throws errors if user already exist

--- internal/seeder/users.go
+++ internal/seeder/users.go
@@ -1,13 +1,26 @@
 package seeder
 
-import "gitlab.com/beewar/beewar-be/internal/controller/auth"
+import (
+	"gitlab.com/beewar/beewar-be/internal/access"
+	"gitlab.com/beewar/beewar-be/internal/controller/auth"
+)
 
 // SeedUsers inserts default users
 func SeedUsers() bool {
-	_ = auth.Register("beebot", "beebot", "beebotbeebot")
-	_ = auth.Register("test1@user.com", "test1", "testtest")
-	_ = auth.Register("test2@user.com", "test2", "testtest")
-	_ = auth.Register("test3@user.com", "test3", "testtest")
-	_ = auth.Register("test4@user.com", "test4", "testtest")
+	if user, _ := access.QueryUserByUsername("beebot"); user == nil {
+		_ = auth.Register("beebot", "beebot", "beebotbeebot")
+	}
+	if user, _ := access.QueryUserByUsername("test1"); user == nil {
+		_ = auth.Register("test1@user.com", "test1", "testtest")
+	}
+	if user, _ := access.QueryUserByUsername("test2"); user == nil {
+		_ = auth.Register("test2@user.com", "test2", "testtest")
+	}
+	if user, _ := access.QueryUserByUsername("test3"); user == nil {
+		_ = auth.Register("test3@user.com", "test3", "testtest")
+	}
+	if user, _ := access.QueryUserByUsername("test4"); user == nil {
+		_ = auth.Register("test4@user.com", "test4", "testtest")
+	}
 	return true
 }
